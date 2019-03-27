679f3015ec6ad32d38d129b833b6586dd0eb41cb
Michael Kasper <43761116+mlkasper@users.noreply.github.com>
2019-03-27 17:21:23 -0500

    Add files via upload

---
+++ src/app/background-color/background-color.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-background-color',
+  templateUrl: './background-color.component.html',
+  styleUrls: ['./background-color.component.css']
+})
+export class BackgroundColorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
